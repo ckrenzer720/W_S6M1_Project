4632843376bc0e2bd2a1a20a693141b3a493cc6d
Conner Krenzer <krenzer84@gmail.com>
2024-08-26 16:53:23 -0400

    'moods finished'

--- frontend/components/Counter.js
+++ frontend/components/Counter.js
@@ -45,8 +45,7 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
-import { useState } from 'react';
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
