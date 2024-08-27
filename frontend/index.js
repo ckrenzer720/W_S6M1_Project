3610afa75ce8c18651117da6f3d1bf06b32533d6
Conner Krenzer <krenzer84@gmail.com>
2024-08-27 10:30:47 -0400

    input

--- frontend/index.js
+++ frontend/index.js
@@ -14,11 +14,11 @@ const root = createRoot(domNode)
 
 root.render(
   <>
-    <Counter />
-    <Moods />
-    <Spinner />
+    {/* <Counter /> */}
+    {/* <Moods /> */}
+    {/* <Spinner /> */}
     <Input />
-    <Squares />
-    <Programmers />
+    {/* <Squares /> */}
+    {/* <Programmers /> */}
   </>
 )
